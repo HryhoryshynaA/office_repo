8fd62f956fd9230a80833609b95b8fbcbdf33b6a
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2026-07-12 12:40:42 +0300

    created function count vowels

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+def count_vowels(text):
+    text = text.lower()
+    vowels = text.count('a') + text.count('u') + text.count('o') + text.count('i') + text.count('e')
+    return vowels
+
+word = "HELLO"
+print(count_vowels(word))
+print(count_vowels("AdrianA"))
