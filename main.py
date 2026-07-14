fcd03db2d312574e717eeb0e1b5500ef99f5fbc3
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2026-07-14 20:33:57 +0300

    clean

--- main.py
+++
@@ -1,8 +0,0 @@
-def count_vowels(text):
-    text = text.lower()
-    vowels = text.count('a') + text.count('u') + text.count('o') + text.count('i') + text.count('e')
-    return vowels
-
-word = "HELLO"
-print(count_vowels(word))
-print(count_vowels("AdrianA"))
